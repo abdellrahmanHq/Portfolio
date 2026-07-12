927ae1a3c67fbc169234353cbf2ba3be0866e539
abdellrahmanHq <hanaqtahabdellrahman@gmail.com>
2026-07-12 13:23:17 +0300

    Fix: move personal image to public folder for production assets resolution

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ export default function Hero() {
             <span className="bracket bracket-bl" />
             <span className="bracket bracket-br" />
             <img
-              src="./src/Personal-image.png"
+              src="/Personal-image.png"
               alt="Abdellrahman AlHanaqtah"
               referrerPolicy="no-referrer"
             />
